941ea7cab43680755333a516f570a97c130ce1ed
Abhijeet <fulpagareabhijeet39@gmail.com>
2026-02-16 18:21:22 +0530

    chore: update backend/index.js

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 connectDB();
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://music-player-kappa-amber.vercel.app",
     credentials:true,
 }))
 
